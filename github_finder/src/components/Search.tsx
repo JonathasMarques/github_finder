c0c0063d92da5a841f74d4af217038a62ff573ca
Jonathas Marques <149964627+JonathasMarques@users.noreply.github.com>
2024-03-26 22:32:07 -0300

    projeto finalizado

--- github_finder/src/components/Search.tsx
+++ github_finder/src/components/Search.tsx
@@ -1,18 +1,39 @@
-import { BsSearch } from "react-icons/bs"
+type SearchProps = {
+	loadUser: (userName: string) => Promise<void>;
+  };
+  
+  import { useState, KeyboardEvent } from "react";
+  
+  import { BsSearch } from "react-icons/bs";
 
-const Search = () => {
-  return (
-	<div>
+  import classes from './Search.module.css'
+  
+  const Search = ({ loadUser }: SearchProps) => {
+	const [userName, setUserName] = useState("");
+  
+	const handleKeyDown = (e: KeyboardEvent) => {
+	  if (e.key === "Enter") {
+		loadUser(userName);
+	  }
+	};
+  
+	return (
+	  <div className={classes.search}>
 		<h2>Busque por um usuário:</h2>
 		<p>Conheça seus melhores repositórios</p>
-		<div>
-			<input type="text" placeholder='Digite o nome do usuário'/>
-			<button>
-				<BsSearch />
-			</button>
+		<div className={classes.search_container}>
+		  <input
+			type="text"
+			onChange={(e) => setUserName(e.target.value)}
+			onKeyDown={handleKeyDown}
+			placeholder="Digite o nome do usuário"
+		  />
+		  <button onClick={() => loadUser(userName)}>
+			<BsSearch />
+		  </button>
 		</div>
-	</div>
-  )
-}
-
-export default Search
+	  </div>
+	);
+  };
+  
+  export default Search;
